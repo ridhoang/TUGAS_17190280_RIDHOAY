f876c2759f69c0347b53d287ae04cb8d412a07d3
ridhoang <ridhoay38@gmail.com>
2022-04-03 14:32:05 +0700

    menambah file latihan 1

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,18 @@
+<?php
+    class Latihan1 extends CI_Controller
+    {
+        public function index()
+        {
+            echo "selamat datang, selamat belajar";
+        }
+
+        
+        public function penjumlahan ( $n1, $n2)
+        
+            {
+                $this->load->model('Model_Latihan1');
+                $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
+                echo "hasil penjumlahan dari ". $n1 ." + " . $n2 . " = " . $hasil;
+            }
+        
+    }
