27fca1570168e5f95c17594fc1ab15cee1f1135c
ridhoang <ridhoay38@gmail.com>
2022-04-03 22:38:55 +0700

    menambahkan tugas contoh 1,2,3 menghilangkan index.php dan parsing data

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -7,12 +7,25 @@
         }
 
         
-        public function penjumlahan ( $n1, $n2)
+        // public function penjumlahan ( $n1, $n2)
+        
+        //     {
+        //         $this->load->model('Model_Latihan1');
+        //         $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
+        //         echo "hasil penjumlahan dari ". $n1 ." + " . $n2 . " = " . $hasil;
+        //     }
+
+            public function penjumlahan ( $n1, $n2)
         
             {
                 $this->load->model('Model_Latihan1');
-                $hasil = $this->Model_Latihan1->jumlah($n1, $n2);
-                echo "hasil penjumlahan dari ". $n1 ." + " . $n2 . " = " . $hasil;
+                
+                $data['nilai1'] = $n1;
+                $data['nilai2'] = $n2;
+                $data['hasil']  = $this->Model_Latihan1->jumlah($n1, $n2);
+
+                $this->load->view('view-Latihan1', $data);
+               
             }
         
     }
---
+++ application/views/view-Latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <h3> hallo gaes .. kita akan belajar web programming </h3>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan method penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?> 
+</body>
+</html>
