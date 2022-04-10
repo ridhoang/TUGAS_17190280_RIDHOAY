73816be6ad5771b95af8f7495d27ef6c4bc07360
ridhoang <ridhoay38@gmail.com>
2022-04-10 13:53:39 +0700

    membuat template sederhana dan setting base url

---
+++ application/views/v_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
+        <link rel="stylesheet" type="text/css" href="http://localhost/pustaka_ku/assets/css/stylebuku.css">
+    </head>
+<body>
+    <div id="wrapper">
+        <header>
+        <hgroup>
+            <h1>RentalBuku.net</h1>
+            <h3>Membuat Template Sederhana dengan 
+            CodeIgniter</h3>
+        </hgroup>
+    <nav>
+    <ul>
+        <li><a href="https://localhost/pustaka_ku/Web/home">Home</a></li>
+        <li><a href="https://localhost/pustaka_ku/Web/about">About</a></li>
+        <li><a href="https://localhost/pustaka_ku/Web/tampiluser">User</a></li>
+        <li><a href="https://localhost/pustaka_ku/Web/login">Login</a></li>
+    </ul>
+    </nav>
+        <div class="clear"></div>
+        </header>
